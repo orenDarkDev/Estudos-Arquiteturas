eb1431d40e6d481fea0bd66543fbfef228b7510f
Oren Pelosof <orenpelosof@gmail.com>
2023-06-27 10:31:26 -0300

    creating a remote repository

---
+++ Arquiteturas/API/Model/UserModel.swift
@@ -0,0 +1,8 @@
+//
+//  UserModel.swift
+//  Arquiteturas
+//
+//  Created by Oren Pelosof on 20/06/23.
+//
+
+import Foundation
---
+++ Arquiteturas/API/Provider/UserProvider.swift
@@ -0,0 +1,16 @@
+//
+//  File.swift
+//  Arquiteturas
+//
+//  Created by Oren Pelosof on 20/06/23.
+//
+
+import Foundation
+
+protocol UserProviderProtocol {
+    
+}
+
+class UserProvider {
+    
+}
